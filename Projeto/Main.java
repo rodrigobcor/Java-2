2361c0ebedcf9179784e57de10b97d6f1ae1ff6f
Rodrigo Bezerra Cordeiro <87913655+rodrigobcor@users.noreply.github.com>
2022-03-06 17:37:33 -0300

    Update Main.java

--- Projeto/Main.java
+++ Projeto/Main.java
@@ -144,17 +144,4 @@ public class Main {
 
 		sc.close();
 	}
-
 }
-
-//		Cliente venilton = new Cliente();
-//		venilton.setNome("Venilton");
-//		
-//		Conta cc = new ContaCorrente(venilton);
-//		Conta poupanca = new ContaPoupanca(venilton);
-//
-//		cc.depositar(100);
-//		cc.transferir(100, poupanca);
-//		
-//		cc.imprimirExtrato();
-//		poupanca.imprimirExtrato();
