01af6075ad50ee7b5fe9f8f870e521321cd322b8
Rodrigo Bezerra Cordeiro <87913655+rodrigobcor@users.noreply.github.com>
2022-03-08 14:24:56 -0300

    final

--- Projeto 2/src/Main.java
+++ Projeto 2/src/Main.java
@@ -2,7 +2,9 @@ package dio_project2;
 
 import java.time.LocalDate;
 
+import dio_project2.dominio.Bootcamp;
 import dio_project2.dominio.Curso;
+import dio_project2.dominio.Dev;
 import dio_project2.dominio.Mentoria;
 
 public class Main {
@@ -22,10 +24,39 @@ public class Main {
 		Mentoria mentoria = new Mentoria();
 		mentoria.setTitulo("mentoria de java");
 		mentoria.setDescricao("descricao mentoria java");
-		mentoria.setData(LocalDate.now());		
+		mentoria.setData(LocalDate.now());
 		
-		System.out.println(curso1);
-		System.out.println(curso2);
-		System.out.println(mentoria);
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev devCamila = new Dev();
+		devCamila.setNome("Camila");
+		devCamila.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos Camila" + devCamila.getConteudosInscritos());
+		devCamila.progredir();
+		devCamila.progredir();
+		System.out.println("Conteudos Inscritos Camila" + devCamila.getConteudosInscritos());
+		System.out.println("Conteudos Concluidos Camila" + devCamila.getConteudosConcluidos());
+		System.out.println("XP:" + devCamila.calcularTotalXP());
+		
+		System.out.println();
+		Dev devJoao = new Dev();
+		devJoao.setNome("Joao");
+		devJoao.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos João" + devJoao.getConteudosInscritos());
+		devJoao.progredir();
+		devJoao.progredir();
+		devJoao.progredir();
+		System.out.println("Conteudos Inscritos João" + devJoao.getConteudosInscritos());
+		System.out.println("Conteudos Concluidos João" + devJoao.getConteudosConcluidos());
+		System.out.println("XP:" + devJoao.calcularTotalXP());
+
+//		System.out.println(curso1);
+//		System.out.println(curso2);
+//		System.out.println(mentoria);
 	}
 }
