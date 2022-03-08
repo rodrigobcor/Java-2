8ded1bcadd072f900b91a887282966827da81015
Rodrigo Bezerra Cordeiro <87913655+rodrigobcor@users.noreply.github.com>
2022-03-08 14:24:46 -0300

    final

--- Projeto 2/src/Curso.java
+++ Projeto 2/src/Curso.java
@@ -1,36 +1,28 @@
 package dio_project2.dominio;
 
-public class Curso {
+public class Curso extends Conteudo {
 
-	private String titulo;
-	private String descricao;
 	private int cargaHoraria;
 	
 	public Curso() {
 	}
-	
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+
 	public int getCargaHoraria() {
 		return cargaHoraria;
 	}
+
 	public void setCargaHoraria(int cargaHoraria) {
 		this.cargaHoraria = cargaHoraria;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
+		return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
+	}
+
+	@Override
+	public double calcularXP() {
+		return XP_PADRAO * cargaHoraria;
 	}
 	
 }
